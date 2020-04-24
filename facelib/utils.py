bd3e4c42d3c907d3349a6017f3521cb0ca15b476
iqDF <daniel.thekurniadi@gmail.com>
2020-04-24 20:10:12 +0800

    added file loading utilities

--- facelib/utils.py
+++ facelib/utils.py
@@ -1,83 +1,43 @@
+import os
+import sys
+import glob
+
 import cv2
 import numpy as np
 
 
-def open_sequence(seq_dir, gray_mode, expand_if_needed=False, max_num_fr=100):
-	""" Opens a sequence of images and expands it to even sizes if necesary
-	Args:
-		fpath: string, path to image sequence
-		gray_mode: boolean, True indicating if images is to be open are in grayscale mode
-		expand_if_needed: if True, the spatial dimensions will be expanded if
-			size is odd
-		expand_axis0: if True, output will have a fourth dimension
-		max_num_fr: maximum number of frames to load
-	Returns:
-		seq: array of dims [num_frames, C, H, W], C=1 grayscale or C=3 RGB, H and W are even.
-			The image gets normalized gets normalized to the range [0, 1].
-		expanded_h: True if original dim H was odd and image got expanded in this dimension.
-		expanded_w: True if original dim W was odd and image got expanded in this dimension.
-	"""
-	# Get ordered list of filenames
-	files = get_imagenames(seq_dir)
-
-	seq_list = []
-	print("\tOpen sequence in folder: ", seq_dir)
-	for fpath in files[0:max_num_fr]:
-
-		img, expanded_h, expanded_w = open_image(fpath,\
-												   gray_mode=gray_mode,\
-												   expand_if_needed=expand_if_needed,\
-												   expand_axis0=False)
-		seq_list.append(img)
-		seq = np.stack(seq_list, axis=0)
-	return seq, expanded_h, expanded_w
+IMAGETYPES = ('*.bmp', '*.png', '*.jpg', '*.jpeg', '*.tif') # Supported image types
 
 
-def open_image(fpath, gray_mode, expand_if_needed=False,
-	expand_axis0=True,normalize_data=True):
-	""" Opens an image and expands it if necesary
-	Args:
-		fpath: string, path of image file
-		gray_mode: boolean, True indicating if image is to be open
-			in grayscale mode
-		expand_if_needed: if True, the spatial dimensions will be expanded if
-			size is odd
-		expand_axis0: if True, output will have a fourth dimension
-	Returns:
-		img: image of dims NxCxHxW, N=1, C=1 grayscale or C=3 RGB, H and W are even.
-			if expand_axis0=False, the output will have a shape CxHxW.
-			The image gets normalized gets normalized to the range [0, 1].
-	"""
-	if not gray_mode:
-		# Open image as a CxHxW image (C=3)
-		img = cv2.imread(fpath)
-	else:
-		# from HxWxC to  CxHxW grayscale image (C=1)
-		img = cv2.imread(fpath, cv2.IMREAD_GRAYSCALE)
-
-	if expand_axis0:
-		img = np.expand_dims(img, 0)
-
-	# Handle odd sizes
-	sh_im = img.shape
-	if expand_if_needed:
-		if sh_im[-2]%2 == 1:
-			if expand_axis0:
-				img = np.concatenate((img, \
-					img[:, :, -1, :][:, :, np.newaxis, :]), axis=2)
-			else:
-				img = np.concatenate((img, \
-					img[:, -1, :][:, np.newaxis, :]), axis=1)
+def get_image_paths(file_root, pattern=None):
+    """ Get ordered list of filepaths
+    """
+    file_paths = []
+    for typ in IMAGETYPES:
+		# search all files conforming to the IMAGE_TYPES extensions
+        file_paths.extend(glob.glob(os.path.join(file_root,'**',typ), recursive=True))
+    # filter filenames
+    if pattern is not None:
+        filtered = []
+        filtered = [f for f in file_paths if pattern in os.path.split(f)[-1]]
+        file_paths = filtered
+        del filtered
+	# sort image file paths
+    file_paths.sort()
+    return file_paths
 
 
-		if sh_im[-1]%2 == 1:
-			if expand_axis0:
-				img = np.concatenate((img, \
-					img[:, :, :, -1][:, :, :, np.newaxis]), axis=3)
-			else:
-				img = np.concatenate((img, \
-					img[:, :, -1][:, :, np.newaxis]), axis=2)
+def load_face_dataset(file_root):
+    """ Load all images and also list out the filepaths
+    """
+    if not os.path.isdir(file_root):
+        raise FileNotFoundError("Face dataset file root: {} not found.".format(file_root))
 
-	if normalize_data:
-		img = normalize(img)
-	return img
+    file_paths = get_image_paths(file_root)
+    
+    if len(file_paths) == 0:
+        warnings.warn("No image found in face dataset file root: {}".format(file_root))
+        return []
+    
+    images = [cv2.imread(file_path) for file_path in file_paths]
+    return images, file_paths
